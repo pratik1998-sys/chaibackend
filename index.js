d9813bd7eec90e33ab8739ec04e93ce5de2cdb7c
pratik <pratiksingh260@gmail.com>
2024-06-19 00:09:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+require("dotenv").config()
+const express = require("express")
+const app = express()
+const port = process.env.PORT
+
+app.get("/", (req, res) => {
+  res.send("Hello World!")
+})
+
+app.get("/twitter", (req, res) => {
+  res.send("pratiksingh260")
+})
+
+app.get("/login", (req, res) => {
+  res.send("<h1>please login at chai aur code</h1>")
+})
+
+app.get("/youtube", (req, res) => {
+  res.send("<h2>chai aur code</h2>")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
